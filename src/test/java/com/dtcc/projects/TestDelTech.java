8ac4001dce415984b7d7e680162e5b3792fea71b
charujamnis <charushila.jamnis@gmail.com>
2020-09-26 20:05:46 -0400

    Test files added

--- src/test/java/com/dtcc/projects/TestDelTech.java
+++ src/test/java/com/dtcc/projects/TestDelTech.java
@@ -1,2 +1,30 @@
-package com.dtcc.projects;public class TestDelTech {
+package com.dtcc.projects;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TestDelTech {
+    DelTech deltech;
+    ArrayList<Instructor> list;
+    @Before
+    public void setup(){
+        deltech=new DelTech();
+        deltech.fireStaff();
+    }
+
+    @Test
+    public void testFireStaff(){
+        Assert.assertEquals(0,DelTech.getInstructors().size());
+    }
+
+    @Test   //Check later
+    public void testHireStaff(){
+            String actual="Jennifer";
+            deltech.hire(new Instructor(actual));
+            list=DelTech.getInstructors();
+            Assert.assertEquals("Jennifer",list.get(0).getInstructorName());
+    }
 }
--- src/test/java/com/dtcc/projects/TestJavaAcademy.java
+++ src/test/java/com/dtcc/projects/TestJavaAcademy.java
@@ -1,2 +1,28 @@
-package com.dtcc.projects;public class TestJavaAcademy {
+package com.dtcc.projects;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestJavaAcademy {
+    JavaAcademy javaAcademy;
+
+    @Before
+    public void setup(){
+        javaAcademy=new JavaAcademy();
+        javaAcademy.removeStudents();
+    }
+
+    @Test
+    public void testRemoveStudents(){
+        Assert.assertTrue(JavaAcademy.getStudents().isEmpty());
+    }
+
+    @Test
+    public void testRecruitStudent(){
+        Student student= new Student("Nisha");
+        JavaAcademy.recruitStudent(student);
+        Assert.assertEquals("Nisha",JavaAcademy.getStudents().get(0).getStudentName());
+    }
 }
+
